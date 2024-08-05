338b6d47db88781de287bc8a4f6b1b5cf92dfd8b
IltamZum <146444132+IltamZum@users.noreply.github.com>
2024-08-06 02:16:44 +0400

    Changed example

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,17 @@ fn main() {
     let source = r#"
         var x = "Hello, From x!";
         var y = 3.14;
-        func greet() {
-            "Hello, world!"
+
+        func add(x, y) {
+            x + y
+        }
+
+        func foo(z, x) {
+            z * add(x, z)
+        }
+
+        func main() {
+            foo(bar, 12)
         }
     "#;
 
